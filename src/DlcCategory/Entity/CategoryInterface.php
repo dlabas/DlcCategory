13be1e4daec0ff8b686c812916e45f44d73e10d5
Daniel Labas <daniel@dlabas.de>
2013-04-24 22:40:45 +0200

    Remove getId() from category interace because it already exists in the node interface

--- src/DlcCategory/Entity/CategoryInterface.php
+++ src/DlcCategory/Entity/CategoryInterface.php
@@ -7,68 +7,61 @@ namespace DlcCategory\Entity;
 interface CategoryInterface
 {
     /**
-     * Getter for $id
-     *
-     * @return int $id
-     */
-    public function getId();
-    
-    /**
      * Setter for $id
      *
      * @param int $id
      */
     public function setId($id);
-    
+
     /**
      * Getter for $name
      *
      * @return string $name
      */
     public function getName();
-    
+
     /**
      * Setter for $name
      *
      * @param string $name
      */
     public function setName($name);
-    
+
     /**
      * Getter for $title
      *
      * @return string $title
      */
     public function getTitle();
-    
+
     /**
      * Setter for $title
      *
      * @param string $title
      */
     public function setTitle($title);
-    
+
     /**
      * Getter for $description
      *
      * @return string $description
      */
     public function getDescription();
-    
+
     /**
      * Setter for $description
      *
      * @param string $description
      */
     public function setDescription($description);
-    
+
     /**
      * Getter for $thumbnail
      *
      * @return string $thumbnail
      */
     public function getThumbnail();
-    
+
     /**
      * Setter for $thumbnail
      *
